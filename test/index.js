b9840d6f0b6e52756284dbee08b3e5332252a610
David Frank <bitinn@gmail.com>
2015-05-18 01:21:12 +0800

    fix 304 issue

--- test/index.js
+++ test/index.js
@@ -2,6 +2,8 @@
 var koa = require('koa')
 var assert = require('assert')
 var request = require('supertest')
+var calculate = require('etag')
+var fs = require('mz/fs')
 
 var sendfile = require('..')
 
@@ -37,23 +39,64 @@ describe('when path exists', function () {
   describe('when GET', function () {
     it('should send the file', function (done) {
       var app = koa()
-      var stats
+      var stats, tag
+      tag = fs.stat(process.cwd() + '/test/fixture.txt')
       app.use(function* (next) {
-        stats = yield* sendfile.call(this, process.cwd() + '/index.js')
+        stats = yield* sendfile.call(this, process.cwd() + '/test/fixture.txt')
         assert.ok(stats)
+        tag = calculate(stats, {
+          weak: true
+        })
       })
 
       request(app.listen())
       .get('/')
       .expect(200)
-      .expect(/module\.exports/, function (err, res) {
+      .expect(/test/, function (err, res) {
         if (err) return done(err)
 
-        assert(/application\/javascript/.test(res.headers['content-type']))
+        assert(/text\/plain/.test(res.headers['content-type']))
         res.headers['content-length'].should.equal(String(stats.size))
         res.headers['last-modified'].should.equal(stats.mtime.toUTCString())
+        res.headers['etag'].should.equal(tag)
 
         done()
+      })
+    })
+
+    it('should support weak etag 304', function (done) {
+      var app = koa()
+      var stats, tag
+      fs.stat(process.cwd() + '/test/fixture.txt').then(function(stat) {
+        tag = calculate(stat, {
+          weak: true
+        })
+
+        app.use(function* (next) {
+          stats = yield* sendfile.call(this, process.cwd() + '/test/fixture.txt')
+          assert.ok(stats)
+        })
+
+        request(app.listen())
+        .get('/')
+        .set('If-None-Match', tag)
+        .expect(304, done)
+      })
+    })
+
+    it('should support last-modified 304', function (done) {
+      var app = koa()
+      var stats
+      fs.stat(process.cwd() + '/test/fixture.txt').then(function(stat) {
+        app.use(function* (next) {
+          stats = yield* sendfile.call(this, process.cwd() + '/test/fixture.txt')
+          assert.ok(stats)
+        })
+
+        request(app.listen())
+        .get('/')
+        .set('If-Modified-Since', stat.mtime.toUTCString())
+        .expect(304, done)
       })
     })
 
@@ -61,7 +104,7 @@ describe('when path exists', function () {
       var app = koa()
       var stream
       app.use(function* (next) {
-        var stats = yield* sendfile.call(this, process.cwd() + '/index.js')
+        var stats = yield* sendfile.call(this, process.cwd() + '/test/fixture.txt')
         assert.ok(stats)
         stream = this.body
         this.socket.emit('error', new Error('boom'))
@@ -81,15 +124,31 @@ describe('when path exists', function () {
     it('should 200', function (done) {
       var app = koa()
       app.use(function* (next) {
-        var stats = yield* sendfile.call(this, process.cwd() + '/index.js')
+        var stats = yield* sendfile.call(this, process.cwd() + '/test/fixture.txt')
         assert.ok(stats)
       })
 
       request(app.listen())
       .head('/')
       .expect(200)
-      .expect('content-type', /application\/javascript/)
+      .expect('content-type', /text\/plain/)
       .expect('', done)
+    })
+
+    it('should 304', function (done) {
+      var app = koa()
+      fs.stat(process.cwd() + '/test/fixture.txt').then(function(stat) {
+        app.use(function* (next) {
+          stats = yield* sendfile.call(this, process.cwd() + '/test/fixture.txt')
+          assert.ok(stats)
+        })
+
+        request(app.listen())
+        .head('/')
+        .set('If-Modified-Since', stat.mtime.toUTCString())
+        .expect(304)
+        .expect('', done)
+      })
     })
   })
 })
